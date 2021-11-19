ac197d4aeff052094cb7817688928bf916a75e8f
Kirk Goddard <hanazaka@comcast.net>
2021-11-19 13:08:04 -0800

    Move the diagnostics screen to it's own file.

--- SSTV/ViewDevice.cs
+++ SSTV/ViewDevice.cs
@@ -19,219 +19,6 @@ using Play.Forms;
 using Play.Sound;
 
 namespace Play.SSTV {
-	/// <summary>
-	/// This view shows the raw scan line with our slot identifiers. Invaluable to spot alignment issues.
-	/// </summary>
-	public class WindowDiagnostics:
-		Control,
-		IPgParent,
-		IPgLoad<XmlElement>,
-		IPgSave<XmlDocumentFragment>,
-		IPgCommandView,
-		IDisposable
-	{
-		public static Guid   GUID { get; }  = new Guid( "{A7F75A46-1800-4605-87EC-2D8B960D1599}" );
-		public static string _strIcon =  "Play.SSTV.icons8_tv.png";
-
-		protected readonly IPgViewSite   _oSiteView;
-		protected readonly DocSSTV       _oDocSSTV;
-
-	  //protected readonly ImageViewSingle _oViewRx;      // Show the currently selected image.
-		protected readonly ImageViewSingle _oViewSync;    // The sync bitmap.
-
-		protected LayoutStack _oLayout = new LayoutStackVertical( 5 );
-
-		protected class SSTVWinSlot :
-			IPgViewSite,
-			IPgShellSite
-		{
-			protected readonly WindowDiagnostics _oHost;
-
-			public SSTVWinSlot( WindowDiagnostics oHost ) {
-				_oHost = oHost ?? throw new ArgumentNullException();
-			}
-
-			public IPgParent Host => _oHost;
-
-			public void LogError(string strMessage, string strDetails, bool fShow=true) {
-				_oHost.LogError( strDetails );
-			}
-
-			public void Notify( ShellNotify eEvent ) {
-				_oHost._oSiteView.Notify( eEvent );
-			}
-
-            public object AddView( Guid guidViewType, bool fFocus ) {
-                return null;
-            }
-
-            public void FocusMe() {
-                throw new NotImplementedException();
-            }
-
-            public void FocusCenterView() {
-                throw new NotImplementedException();
-            }
-
-            public IPgViewNotify EventChain => _oHost._oSiteView.EventChain;
-
-            public IEnumerable<IPgCommandView> EnumerateSiblings => throw new NotImplementedException();
-
-            public uint SiteID => throw new NotImplementedException();
-        }
-
-		public IPgParent Parentage => _oSiteView.Host;
-		public IPgParent Services  => Parentage.Services;
-		public bool      IsDirty   => false;
-		public Image     Iconic    { get; }
-		public Guid      Catagory  => GUID;
-
-        public string Banner {
-			get { 
-				StringBuilder sbBanner = new StringBuilder();
-
-				sbBanner.Append( _strBaseTitle );
-				if( _oDocSSTV.PortRxList.CheckedLine is Line oLine ) {
-					sbBanner.Append( " : " );
-					sbBanner.Append( oLine.ToString() );
-				}
-
-				return sbBanner.ToString();
-			} 
-		}
-		protected readonly string _strBaseTitle = "Diagnostics Window : ";
-
-        public WindowDiagnostics( IPgViewSite oViewSite, DocSSTV oDocument ) {
-			_oSiteView = oViewSite ?? throw new ArgumentNullException( "View requires a view site." );
-			_oDocSSTV  = oDocument ?? throw new ArgumentNullException( "View requires a document." );
-
-			Iconic = ImageResourceHelper.GetImageResource( Assembly.GetExecutingAssembly(), _strIcon );
-
-		  //_oViewRx   = new ImageViewSingle( new SSTVWinSlot( this ), _oDocSSTV.ReceiveImage );
-			_oViewSync = new ImageViewSingle( new SSTVWinSlot( this ), _oDocSSTV.SyncImage );
-			_oViewSync.SetBorderOn();
-		}
-
-		protected override void Dispose( bool disposing ) {
-			_oDocSSTV.PropertyChange -= Listen_PropertyChange;
-
-			base.Dispose( disposing );
-		}
-
-		public void LogError( string strMessage ) {
-			_oSiteView.LogError( "SSTV View", strMessage );
-		}
-
-		public bool InitNew() {
-			if( !_oViewSync.InitNew() )
-				return false;
-			//if( !_oViewRx.InitNew() )
-			//	return false;
-
-			//_oViewRx  .Parent = this;
-			_oViewSync.Parent = this;
-
-            _oDocSSTV.PropertyChange += Listen_PropertyChange;
-
-            //_oLayout.Add( new LayoutControl( _oViewRx,    LayoutRect.CSS.Percent, 60 ) );
-            _oLayout.Add( new LayoutControl( _oViewSync , LayoutRect.CSS.Percent, 100 ) );
-
-            OnSizeChanged( new EventArgs() );
-
-			return true;
-		}
-
-        /// <summary>
-        /// This is our event sink for property changes on the SSTV document.
-        /// </summary>
-        /// <remarks>Right now just update all, but we can just update the
-        /// specific property in the future. You know, a color coded property, 
-        /// light red or yellow on change would be a cool feature.</remarks>
-        private void Listen_PropertyChange( SSTVEvents eProp ) {
-			switch( eProp ) {
-				case SSTVEvents.DownLoadFinished:
-				  //_oViewRx  .Refresh();
-					_oViewSync.Refresh();
-					break;
-				default:
-					Invalidate();
-					_oSiteView.Notify( ShellNotify.BannerChanged );
-					break;
-			}
-        }
-
-        public bool Load(XmlElement oStream) {
-			InitNew();
-			return true;
-		}
-
-		public bool Save(XmlDocumentFragment oStream) {
-			return true;
-		}
-
-		protected override void OnSizeChanged(EventArgs e) {
-			base.OnSizeChanged(e);
-
-			_oLayout.SetRect( 0, 0, Width, Height );
-			_oLayout.LayoutChildren();
-
-            Invalidate();
-		}
-
-		public object Decorate(IPgViewSite oBaseSite,Guid sGuid) {
-			try {
-				if( sGuid.Equals(GlobalDecorations.Properties) ) {
-					return new ViewSSTVProperties( oBaseSite, _oDocSSTV.RxProperties );
-				}
-				return false;
-			} catch ( Exception oEx ) {
-				Type[] rgErrors = { typeof( NotImplementedException ),
-									typeof( NullReferenceException ),
-									typeof( ArgumentException ),
-									typeof( ArgumentNullException ) };
-				if( rgErrors.IsUnhandled( oEx ) )
-					throw;
-
-				LogError( "Couldn't create SSTV decor: " + sGuid.ToString() );
-			}
-
-            return( null );
-		}
-
-		public bool Execute(Guid sGuid) {
-			if( sGuid == GlobalCommands.Stop ) {
-				_oDocSSTV.TransmitStop(); // Stop record or play.
-				return true;
-			}
-
-			return false;
-		}
-
-        protected override void OnGotFocus( EventArgs e ) {
-            base.OnGotFocus(e);
-			_oSiteView.EventChain.NotifyFocused( true );
-			Invalidate();
-        }
-
-        protected override void OnLostFocus( EventArgs e ) {
-            base.OnGotFocus(e);
-			_oSiteView.EventChain.NotifyFocused( false );
-			Invalidate();
-        }
-        protected override void OnKeyDown(KeyEventArgs e) {
-            if( this.IsDisposed )
-                return;
-
-            e.Handled = true;
-
-            switch( e.KeyCode ) {
-                case Keys.Right:
-                case Keys.Enter:
-                    break;
-            }
-        }
-    }
-
     /// <summary>
 	/// We'll turn that into optionally a dropdown in the future.
     /// </summary>
@@ -779,8 +566,7 @@ namespace Play.SSTV {
                     throw;
             }
 		}
-
-	}
+	} // End WindowRxBase
 
 	/// <summary>
 	/// Spiffy new window that shows the receive directory as icons.
@@ -939,7 +725,8 @@ namespace Play.SSTV {
     }
 
 	/// <summary>
-	/// Layout the rect's inside in a stagagared manner.
+	/// Layout the rect's inside in a stagagared manner. Going to move this to
+	/// the Drawing project once things settle down.
 	/// </summary>
 	/// <seealso cref="LayoutStack"/>
 	public class LayoutStaggared : ParentRect {
