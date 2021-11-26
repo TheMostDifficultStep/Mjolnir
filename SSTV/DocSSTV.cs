c6495239a966533134cca99d725d49ee65d43905
Foo S Bar <horseflyrider@gmail.com>
2021-11-26 15:56:02 -0800

    Make the send resolution and time listing a little tighter together.

--- SSTV/DocSSTV.cs
+++ SSTV/DocSSTV.cs
@@ -473,11 +473,11 @@ namespace Play.SSTV {
                 if( fAddResolution ) {
                     sbValue.Append( " : " );
                     sbValue.Append( oMode.Resolution.Width.ToString() );
-                    sbValue.Append( " x " );
+                    sbValue.Append( "x" );
                     sbValue.Append( oMode.Resolution.Height.ToString() );
                     sbValue.Append( " @ " );
                     sbValue.Append( ( oMode.ScanWidthInMS * oMode.Resolution.Height / oMode.ScanMultiplier / 1000 ).ToString( "0." ) );
-                    sbValue.Append( " s" );
+                    sbValue.Append( "s" );
                 }
                 Line oLine = oBulk.LineAppendNoUndo( sbValue.ToString() );
 
