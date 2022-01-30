8d9fcd8adb8a561a92fb1ba0104e0673ed3d203b
Kirk Goddard <hanazaka@comcast.net>
2022-01-30 11:59:24 -0800

    Add some padding to the bottom of the main screen

--- Mjolnir/MainWin.cs
+++ Mjolnir/MainWin.cs
@@ -339,6 +339,8 @@ namespace Mjolnir {
             _oLayoutPrimary.Add( oCenter); 
             _oLayoutPrimary.Add( _rgSideInfo[SideIdentify.Bottom] );
 
+            _oLayoutPrimary.Padding.Bottom = 5;
+
             DecorMenuReload();
         }
 
