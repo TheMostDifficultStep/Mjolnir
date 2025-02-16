1258c0a301b21c26189fdd7da57c6a78384f3e85
Foo S Bar <horseflyrider@gmail.com>
2025-02-16 12:24:11 -0800

    Go ahead and add my little scanner experiment. Alas, scanner doesn't show up in USB list.

---
+++ Scanner/Controller.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Play.Interfaces.Embedding; 
+using Play.Edit;
+using Play.ImageViewer;
+
+namespace Scanner {
+    public class ScannerController : 
+        Controller 
+    {
+        public ScannerController() {
+			_rgExtensions.Add( ".scan" );
+        }
+
+        public override IDisposable CreateDocument( IPgBaseSite oSite, string strExtension ) {
+			if( strExtension.ToLower() == ".scan" ) {
+				return new DocScanner( oSite );
+			}
+
+			return null;
+        }
+
+        public override IDisposable CreateView( IPgViewSite oBaseSite, object oDocument, Guid guidViewType ) {
+			try {
+                DocScanner oDocNetHost = (DocScanner)oDocument;
+
+                switch( guidViewType ) {
+                    default:
+                        return new WindowSoloImage(oBaseSite, oDocNetHost );
+                }
+            } catch( Exception oEx ) {
+                Type[] rgErrors = { typeof( NullReferenceException ),
+                                    typeof( InvalidCastException ),
+                                    typeof( ArgumentNullException ),
+									typeof( ArgumentException ) };
+                if( rgErrors.IsUnhandled( oEx ) )
+                    throw;
+
+				throw new InvalidOperationException( "Controller couldn't create view for Image document.", oEx );
+            }
+        }
+
+        public override IEnumerator<IPgViewType> GetEnumerator() {
+            yield return new ViewType( "Image", Guid.Empty );
+        }
+
+        public override PgDocDescr Suitability(string strExtension) {
+            foreach( string strExtn in _rgExtensions ) {
+                if( string.Compare( strExtn, strExtension, ignoreCase:true ) == 0 ) {
+                    return new PgDocDescr( strExtension, 
+                                           typeof( IPgLoad<BinaryReader> ), 
+                                           200, 
+                                           this );
+                }
+            }
+            return new PgDocDescr( strExtension, 
+                                   typeof( IPgLoad<BinaryReader>  ), 
+                                   0, 
+                                   this );
+        }
+    }
+}
---
+++ Scanner/DocScanner.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using System.Collections.ObjectModel;
+
+using Play.ImageViewer;
+using Play.Interfaces.Embedding;
+
+using LibUsbDotNet;
+using LibUsbDotNet.Info;
+using LibUsbDotNet.Main;
+
+namespace Scanner {
+    public class DocScanner :
+        ImageSoloDoc,
+        IPgLoad<BinaryReader>
+    {
+        public DocScanner(IPgBaseSite oSiteBase) : base(oSiteBase) {
+        }
+
+        public static UsbDevice MyUsbDevice;
+
+        // https://libusbdotnet.sourceforge.net/V2/Index.html
+		protected override bool Initialize() {
+            List<string> rgResults = new List<string>();
+
+            // Alas, my scanner does not show up in this list. It seems to
+            // use usbscan.sys instead of winusb.sys like my tablet does. 
+            // Dump all devices and descriptor information to console output.
+            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+            foreach (UsbRegistry usbRegistry in allDevices) {
+                if (usbRegistry.Open(out MyUsbDevice)) {
+                    rgResults.Add(MyUsbDevice.Info.ToString());
+                    for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
+                    {
+                        UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
+                        rgResults.Add(configInfo.ToString());
+
+                        ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
+                        for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
+                        {
+                            UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
+                            rgResults.Add(interfaceInfo.ToString());
+
+                            ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
+                            for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
+                            {
+                                rgResults.Add(endpointList[iEndpoint].ToString());
+                            }
+                        }
+                    }
+                }
+            }
+
+
+ 			return true;
+		}
+
+        public bool Load(BinaryReader oStream) {
+            return Initialize();
+        }
+    } // end class
+}
