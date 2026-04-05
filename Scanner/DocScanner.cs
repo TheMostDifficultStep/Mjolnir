0fd29f01896370d3a719c7c0f2ed174a20bacfe3
Foo S Bar <horseflyrider@gmail.com>
2026-04-04 23:21:38 -0700

    Give up on scanners for the moment.

--- Scanner/DocScanner.cs
+++ Scanner/DocScanner.cs
@@ -5,9 +5,10 @@ using System.Collections.ObjectModel;
 using Play.ImageViewer;
 using Play.Interfaces.Embedding;
 
-using LibUsbDotNet;
-using LibUsbDotNet.Info;
-using LibUsbDotNet.Main;
+// was trying out libusbdotnet. But it doesn't work with my epson scanner. :-/
+//using LibUsbDotNet;
+//using LibUsbDotNet.Info;
+//using LibUsbDotNet.Main;
 
 namespace Scanner {
     public class DocScanner :
@@ -17,39 +18,39 @@ namespace Scanner {
         public DocScanner(IPgBaseSite oSiteBase) : base(oSiteBase) {
         }
 
-        public static UsbDevice MyUsbDevice;
+        //public static UsbDevice MyUsbDevice;
 
         // https://libusbdotnet.sourceforge.net/V2/Index.html
 		protected override bool Initialize() {
-            List<string> rgResults = new List<string>();
+            //List<string> rgResults = new List<string>();
 
-            // Alas, my scanner does not show up in this list. It seems to
-            // use usbscan.sys instead of winusb.sys like my tablet does. 
-            // Dump all devices and descriptor information to console output.
-            UsbRegDeviceList allDevices = UsbDevice.AllDevices;
-            foreach (UsbRegistry usbRegistry in allDevices) {
-                if (usbRegistry.Open(out MyUsbDevice)) {
-                    rgResults.Add(MyUsbDevice.Info.ToString());
-                    for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
-                    {
-                        UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
-                        rgResults.Add(configInfo.ToString());
+            //// Alas, my scanner does not show up in this list. It seems to
+            //// use usbscan.sys instead of winusb.sys like my tablet does. 
+            //// Dump all devices and descriptor information to console output.
+            //UsbRegDeviceList allDevices = UsbDevice.AllDevices;
+            //foreach (UsbRegistry usbRegistry in allDevices) {
+            //    if (usbRegistry.Open(out MyUsbDevice)) {
+            //        rgResults.Add(MyUsbDevice.Info.ToString());
+            //        for (int iConfig = 0; iConfig < MyUsbDevice.Configs.Count; iConfig++)
+            //        {
+            //            UsbConfigInfo configInfo = MyUsbDevice.Configs[iConfig];
+            //            rgResults.Add(configInfo.ToString());
 
-                        ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
-                        for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
-                        {
-                            UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
-                            rgResults.Add(interfaceInfo.ToString());
+            //            ReadOnlyCollection<UsbInterfaceInfo> interfaceList = configInfo.InterfaceInfoList;
+            //            for (int iInterface = 0; iInterface < interfaceList.Count; iInterface++)
+            //            {
+            //                UsbInterfaceInfo interfaceInfo = interfaceList[iInterface];
+            //                rgResults.Add(interfaceInfo.ToString());
 
-                            ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
-                            for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
-                            {
-                                rgResults.Add(endpointList[iEndpoint].ToString());
-                            }
-                        }
-                    }
-                }
-            }
+            //                ReadOnlyCollection<UsbEndpointInfo> endpointList = interfaceInfo.EndpointInfoList;
+            //                for (int iEndpoint = 0; iEndpoint < endpointList.Count; iEndpoint++)
+            //                {
+            //                    rgResults.Add(endpointList[iEndpoint].ToString());
+            //                }
+            //            }
+            //        }
+            //    }
+            //}
 
 
  			return true;
